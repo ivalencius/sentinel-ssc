07e04276f725edb653604f5c280038c9eedf9749
Ilan Valencius <ilanvalencius@gmail.com>
2025-05-19 14:16:47 -0400

    Updated figures

--- create_figs.R
+++ create_figs.R
@@ -21,7 +21,7 @@ library(extrafont)
 # font_import()
 
 # Working Directory
-wd_root <- "/Users/ilanvalencius/Documents/River-Sed-Manuscript"
+wd_root <- "/Users/ilanvalencius/Documents/River-Sed-Manuscript/sentinel-ssc"
 setwd(wd_root)
 # Exports folder
 wd_imports <- paste0(wd_root, "/figure-data/")
@@ -34,9 +34,8 @@ fancy_scientific <- function(l) {
   return(parse(text = paste("10^", as.character(l), sep = "")))
 }
 ##################  Significance of Regression  ################################
-df <- read.csv('/Users/ilanvalencius/Documents/River-Sed-Manuscript/figure-data/RATING_regression/holdout_data.csv')
+df <- read.csv('/Users/ilanvalencius/Documents/River-Sed-Manuscript/sentinel-ssc/figure-data/RATING_regression/holdout_data.csv')
 
-# Number of animals in each group 
 n = nrow(df)
 names=c(rep("Holdout", n) , rep("Predicted", n))
 # Draw samples from two different populations
@@ -62,7 +61,7 @@ ggplot(stations, aes(x = year, y = n_stations)) +
   geom_point(aes(y=n_samples/coeff), color = col2) +
   labs(x = "Year", y = "Number of USGS-SSC Monitoring Sites") +
   theme_bw() +
-  theme(text=element_text(family="JetBrains Mono NL")) +
+  #theme(text=element_text(family="JetBrains Mono NL")) +
   scale_y_continuous(
      labels = scales::comma,
     # Add a second axis and specify its features
@@ -103,7 +102,7 @@ samples_summed <- rating_samples %>%
     mean_lag = mean(LAG_DAYS)
   )
 
-stations <- left_join(samples_summed, latlon, by = "SITE_NO")
+stations <- left_join(samples_summed, latlon, by = "SITE_NO") %>%
   filter(is.finite(LAT))
 
 # Get basemap
@@ -127,10 +126,11 @@ mean_ssc_map <- ggplot(data = world) +
     theme_bw() +
     labs(x = "Longitude", y = "Latitude") +
     theme(legend.position = c(0.92, 0.2), legend.direction = "vertical") +
-    theme(legend.box.background = element_rect(colour = "black", size=1)) +
-    theme(text = element_text(family = "JetBrains Mono NL"))
+    theme(legend.box.background = element_rect(colour = "black", size=1))  +
+    theme(legend.key.size = unit(0.2, "cm"))
+    #theme(text = element_text(family = "JetBrains Mono NL"))
 
-# ggsave(filename = paste0(wd_exports, "sites-mean-ssc-main.png"))
+ggsave(filename = paste0(wd_exports, "sites-mean-ssc-main.png"))
 
 # ggplot(data = world) +
 #     geom_sf() +
@@ -170,13 +170,14 @@ n_samp_map <- ggplot(data = world) +
     labs(x = "Longitude", y = "Latitude") +
     theme(legend.position = c(0.92, 0.2), legend.direction = "vertical") +
     theme(legend.box.background = element_rect(colour = "black", size=1)) +
+    theme(legend.key.size = unit(0.2, "cm"))
     # theme(legend.text = element_text(size = rel(2))) +
     # theme(legend.title = element_text(size = rel(3))) +
     # theme(axis.text = element_text(size = rel(2.5))) +
     # theme(axis.title = element_text(size = rel(4))) 
-    theme(text = element_text(family = "JetBrains Mono NL"))
+    #theme(text = element_text(family = "JetBrains Mono NL"))
 
-# ggsave(filename = paste0(wd_exports, "sites-n-samp-main.png"))
+ggsave(filename = paste0(wd_exports, "sites-n-samp-main.png"))
 
 # ggplot(data = world) +
 #     geom_sf() +
@@ -202,14 +203,15 @@ ggplot(data = world) +
     geom_sf() +
     geom_sf(data=rivers50, color="blue", size=0.5, alpha=0.5) +
     geom_sf(data = states, fill = NA) +
-    geom_point(data = stations, aes(x = lon, y = lat, fill=factor(cluster)), size = 4, 
+    geom_point(data = stations, aes(x = LON, y = LAT, fill=factor(cluster)), size = 4, 
         shape = 23) +
     coord_sf(xlim = c(-125, -67), ylim = c(25, 50), expand = FALSE) +
     theme_bw() +
     labs(x = "Longitude", y = "Latitude") +
     theme(legend.position = c(0.92, 0.2), legend.direction = "vertical") +
     theme(legend.box.background = element_rect(colour = "black", size=1)) +
-    theme(text = element_text(family = "JetBrains Mono NL")) +
+    theme(legend.key.size = unit(0.1, "cm")) +
+    #theme(text = element_text(family = "JetBrains Mono NL")) +
     labs(fill = "River Cluster")
 
 ggsave(filename = paste0(wd_exports, "sites-cluster-main.png"))
@@ -233,19 +235,19 @@ samples <- read.csv(paste0(wd_imports, "lag_4_INSITU_CLUSTERED.csv"))
 rating_samples <- read.csv(paste0(wd_imports, "lag_4_RATING_CLUSTERED.csv"))
 
 sample_hist <- ggplot(rating_samples, aes(log10(SSC_MGL), fill="Rating Curve Derived")) +
-  geom_histogram(col="black", bins=40, alpha=0.8) +
+  geom_histogram(col="black", bins=40, alpha=1) +
   geom_histogram(data=samples, aes(fill="In-Situ Samples"), col="black", bins=40, alpha=1) +
   theme_bw() +
-  theme(legend.position = c(0.75, 0.15)) +
+  theme(legend.position = c(0.75, 0.75)) +
   theme(legend.box.background = element_rect(colour = "black", size=1)) +
-  labs(x = "Suspended Sediment Concentration [log10(mg/L)]", y = "Number of Samples", fill = "Data Source") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+  labs(x = "Suspended Sediment Concentration [log10(mg/L)]", y = "Number of Samples", fill = "Data Source")
+  #theme(text = element_text(family = "JetBrains Mono NL"))
 
-# ggsave(
-#   filename = paste0(wd_exports, "samples-hist.png"),
-#   height = 5,
-#   width = 5,
-#   unit = "in")
+ggsave(
+   filename = paste0(wd_exports, "samples-hist.png"),
+   height = 5,
+   width = 5,
+   unit = "in")
 
 ###################   Figure 2 - Rating Curve  #################################
 ratingCurveData <- read.csv(paste0(wd_imports, "RATINGCURVE_SSC.csv"))
@@ -264,27 +266,25 @@ rating_curve <- ggplot(insitu_rating_data, aes(x = LOG10_Q_M3S, y = LOG10_SSC_FL
   labs(
     x = "River Discharge [log10(m^3/s)]",
     y = "Suspended Sediment Flux [log10(Mt/yr)]",
-    title = paste0("USGS Station ", station_sel, " (R2=", ex_R2,")")) +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+    title = paste0("USGS Station ", station_sel, " (R^2=", ex_R2,")"))
+  #theme(text = element_text(family = "JetBrains Mono NL"))
 
-# ggsave(
-#   filename = paste0(wd_exports, "rating-curve.png"),
-#   height = 5,
-#   width = 5,
-#   units = "in")
+ggsave(
+   filename = paste0(wd_exports, "rating-curve.png"),
+   height = 5,
+   width = 5,
+   units = "in")
 
 ################################################################################
-
 # Arrange all figures
 ggarrange(
   n_samp_map, rating_curve, mean_ssc_map, sample_hist,
   widths = c(2, 1), ncol = 2, nrow  = 2, 
-  labels = c('a','b','c','d'))
+  labels = c('(a)','(b)','(c)','(d)'))
 
-# 25 x 18 aspect ratio roughly
-ggsave(filename = paste0(wd_exports, "fig2.png"), height = 10, width = 13.8, units = "in")
+ggsave(filename = paste0(wd_exports, "fig2.png"), height = 8, width = 12, units = "in", bg='white')
 
-###################   Figure 2 - Rating Curve  #################################
+###################   Figure 3 - Color  #################################
 df <- read.csv(paste0(wd_imports, "lag_4_RATING_CLUSTERED.csv"))
 # ssc_categories <- c(0,50,100,250,500,750,1e6)
 ssc_categories <- c(0,50,100,200,500,1e6)
@@ -336,8 +336,8 @@ ggplot(ssc_category_color, aes(x = cluster_sel, y = ssc_category)) +
   scale_x_continuous(expand = c(0, 0)) +
   scale_y_discrete(expand = c(0, 0)) +
   # Remove background grid
-  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
-  theme(text=element_text(family="JetBrains Mono NL"))
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  #theme(text=element_text(family="JetBrains Mono NL"))
 
 ggsave(
   filename = paste0(wd_exports, "cluster-groupings.png"),
@@ -350,7 +350,7 @@ ggsave(
 # Step 1 - Need to generate predictions for all sites
 # holdout <- read.csv("figure-data/RATING_regression/holdout_data.csv") %>% select(-starts_with("B"))
 # df <- read.csv("figure-data/all_ssc_pred.csv")
-df <- read.csv('/Users/ilanvalencius/Documents/River-Sed-Manuscript/figure-data/RATING_regression/holdout_data.csv')
+df <- read.csv('/Users/ilanvalencius/Documents/River-Sed-Manuscript/sentinel-ssc/figure-data/RATING_regression/holdout_data.csv')
 df$LOG10_RESIDUAL <- df$PRED_LOG10_SSC_MGL - df$LOG10_SSC_MGL
 # Residuals to do
 # Width 
@@ -378,8 +378,8 @@ r_width <- ggplot(df %>% filter(MERIT_WIDTH_M >= 60), aes(x = log10(MERIT_WIDTH_
   geom_hline(yintercept = 0, linetype="dashed", color = "Red") +
   labs(
     x = "MERIT Hydro River Width [log10(m)]",
-    y = "Model Residual\n[log10(predicted SSC) - log10(training sample SSC)]") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+    y = "Model Residual\n[log10(predicted SSC) - log10(training sample SSC)]")
+  #theme(text = element_text(family = "JetBrains Mono NL"))
 
 # ggplot(df, aes(x = SAMPLE_DEPTH_M, y = LOG10_RESIDUAL)) +
 #   geom_point(color = "black", alpha = 0.3) +
@@ -412,8 +412,8 @@ r_cirrus <- ggplot(df %>% filter(THIN_CIRRUS_PERCENTAGE > 10^-6), aes(x = log10(
   geom_hline(yintercept = 0, linetype="dashed", color = "Red") +
   labs(
     x = "Thin Cirrus Percentage [log10(%)]",
-    y = "Model Residual\n[log10(predicted SSC) - log10(training sample SSC)]") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+    y = "Model Residual\n[log10(predicted SSC) - log10(training sample SSC)]")
+  #heme(text = element_text(family = "JetBrains Mono NL"))
 
 
 r_drainage <- ggplot(df, aes(x = log10(DRAINAGE_KM2), y = LOG10_RESIDUAL)) +
@@ -435,8 +435,8 @@ r_drainage <- ggplot(df, aes(x = log10(DRAINAGE_KM2), y = LOG10_RESIDUAL)) +
   geom_hline(yintercept = 0, linetype="dashed", color = "Red") +
   labs(
     x = "Drainage Area [log10(km^2)]",
-    y = "Model Residual\n[log10(predicted SSC) - log10(training sample SSC)]") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+    y = "Model Residual\n[log10(predicted SSC) - log10(training sample SSC)]")
+  #heme(text = element_text(family = "JetBrains Mono NL"))
 
 df_factor <- df %>% mutate(LAG_DAYS = factor(abs(LAG_DAYS)),TYPE = factor(TYPE),
 CLUSTER = factor(CLUSTER), SOURCE = factor(SOURCE),
@@ -471,16 +471,16 @@ r_boxplot <- ggplot(df_factor, aes(x = SOURCE, y = LOG10_RESIDUAL)) +
   geom_hline(yintercept = 0, linetype="dashed",  color = "Red") +
   labs(
     x = "Data Source",
-    y = "Model Residual\n[log10(predicted SSC) - log10(training sample SSC)]") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+    y = "Model Residual\n[log10(predicted SSC) - log10(training sample SSC)]")
+  #theme(text = element_text(family = "JetBrains Mono NL"))
 
-ggarrange(r_width, r_cirrus, r_drainage, r_boxplot, ncol = 2, nrow = 2, labels = c('a','b','c','d'))
+ggarrange(r_width, r_cirrus, r_drainage, r_boxplot, ncol = 2, nrow = 2, labels = c('(a)','(b)','(c)','(d)'))
 
-ggsave(filename = paste0(wd_exports, "regression-residuals.png"), height = 12, width = 12, units = "in")
+ggsave(filename = paste0(wd_exports, "regression-residuals.png"), height = 10, width = 10, units = "in")
 
 ###################### Fig 5 - Model Bias ######################################
 # df <- read.csv("figure-data/all_ssc_pred.csv")
-df <- read.csv('/Users/ilanvalencius/Documents/River-Sed-Manuscript/figure-data/RATING_regression/holdout_data.csv')
+df <- read.csv('/Users/ilanvalencius/Documents/River-Sed-Manuscript/sentinel-ssc/figure-data/RATING_regression/holdout_data.csv')
 # Investigate bias
 totalBias <- function(df) {
   return(10^median(abs(log10(10^df$PRED_LOG10_SSC_MGL/10^df$LOG10_SSC_MGL)), na.rm = T)-1)
@@ -488,49 +488,62 @@ totalBias <- function(df) {
 totalBias(df)
 
 holdout <- df
-# holdout$BIAS <- abs(log10(10^holdout$PRED_LOG10_SSC_MGL/10^holdout$LOG10_SSC_MGL))-1)
+holdout$BIAS <- (10^abs(log10(10^holdout$PRED_LOG10_SSC_MGL/10^holdout$LOG10_SSC_MGL)))-1
+#abs(log10(10^holdout$PRED_LOG10_SSC_MGL/10^holdout$LOG10_SSC_MGL)-1)
 # One invalid bias number - remove
-# holdout <- holdout %>% filter(is.finite(BIAS))
+holdout <- holdout %>% filter(is.finite(BIAS))
 
 holdout_bias <- ggplot(holdout, aes(
-  y = (10^abs(log10(10^PRED_LOG10_SSC_MGL/10^LOG10_SSC_MGL)))-1, 
+  y = BIAS, 
   x="")) + 
-  geom_boxplot(color = "#1B6B1B") + 
+  #geom_boxplot(color = "#1B6B1B") + 
+  geom_boxplot(color='black') +
   # geom_jitter(width = 0.1, alpha = 0.3) +
   # coord_cartesian(ylim = c(0.001, 200)) +
   scale_y_log10(limits = c(0.001, 200), labels = fancy_scientific) + 
   theme_bw() +
   guides(color = "none") +
   geom_hline(yintercept = 0, linetype="dashed",  color = "Red") +
-  labs(x = "Holdout Data", y = "Relative Bias") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+  labs(x = "Holdout Data", y = "Relative Bias")
+  #theme(text = element_text(family = "JetBrains Mono NL"))
 
-cluster_bias <- ggplot(holdout, aes(
-  y = (10^abs(log10(10^PRED_LOG10_SSC_MGL/10^LOG10_SSC_MGL)))-1, 
-  x=factor(CLUSTER))) + 
-  geom_boxplot(aes(color = factor(SOURCE)), position = position_dodge(preserve = "single")) + 
-  # geom_jitter(width = 0.1, alpha = 0.3) +
-  # coord_cartesian(ylim = c(0.001, 200)) +
-  scale_y_log10(limits = c(0.001, 200), labels = fancy_scientific) + 
-  theme_bw() +
-  # guides(color = "none") +
-  scale_color_manual(values = c("#440154FF", "#2A788EFF", 
-  "#7AD151FF")) + 
-  theme(legend.position = c(0.8, 0.1)) +
-  geom_hline(yintercept = 0, linetype="dashed",  color = "Red") +
-  labs(x = "CLUSTER", y = "", color = "Data Source") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+# cluster_bias <- ggplot(holdout, aes(
+#   y = BIAS, 
+#   x=factor(CLUSTER))) + 
+#   geom_boxplot(aes(color = factor(SOURCE)), position = position_dodge(preserve = "single")) + 
+#   # geom_jitter(width = 0.1, alpha = 0.3) +
+#   # coord_cartesian(ylim = c(0.001, 200)) +
+#   scale_y_log10(limits = c(0.001, 200), labels = fancy_scientific) + 
+#   theme_bw() +
+#   # guides(color = "none") +
+#   scale_color_manual(values = c("#440154FF", "#2A788EFF", 
+#   "#7AD151FF")) + 
+#   theme(legend.position = c(0.8, 0.1)) +
+#   geom_hline(yintercept = 0, linetype="dashed",  color = "Red") +
+#   labs(x = "CLUSTER", y = "", color = "Data Source")
+#   #theme(text = element_text(family = "JetBrains Mono NL"))
+
+lag_bias <- ggplot(holdout, aes(y = BIAS, x=factor(abs(LAG_DAYS)))) +
+    geom_boxplot(color='black', position = position_dodge(preserve = "single")) +
+    # geom_jitter(width = 0.1, alpha = 0.3) +
+    # coord_cartesian(ylim = c(0.001, 200)) +
+    scale_y_log10(limits = c(0.001, 200), labels = fancy_scientific) +
+    theme_bw() +
+    # guides(color = "none") +
+    theme(legend.position = "None") +
+    geom_hline(yintercept = 0, linetype="dashed",  color = "Red") +
+    labs(x = "|Lag Days|", y = "", color = "Data Source")
 
 
-ggarrange(holdout_bias, cluster_bias, ncol = 2, widths=c(1,3))
+ggarrange(holdout_bias, lag_bias, ncol = 2, widths=c(1,3), labels = c('(a)','(b)'))
 
-ggsave(filename=paste0(wd_exports, "cluster-bias.png"), height = 7, width = 10, units = "in")
+ggsave(filename=paste0(wd_exports, "lag-bias.png"), height = 7, width = 10, units = "in")
 
 ###################### Fig 6 - Chattahoochee ###################################
 # Majority cluster
-chatM <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/figure-data/CHAT4.csv")
+chatM <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/sentinel-ssc/figure-data/CHAT4.csv")
 # Variable cluster
-chatV <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/figure-data/CHATVAR.csv")
+chatV <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/sentinel-ssc/figure-data/CHATVAR.csv")
 
 chatM <- chatM %>% group_by(DISTANCE_KM) %>% 
   reframe(
@@ -561,8 +574,8 @@ m <- ggplot(chatM, aes(x=DISTANCE_KM, y=M)) +
   labs(
     x = "Distance Downstream of Buford Dam [km]",
     y = "SSC [mg/L]",
-    title="Majority Cluster") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+    title="Majority Cluster")
+  #theme(text = element_text(family = "JetBrains Mono NL"))
 
 v <- ggplot(chatV, aes(x=DISTANCE_KM, y=M)) +
   geom_ribbon(aes(xmin=DISTANCE_KM, xmax=d2, ymin=L, ymax=H, fill="Variable Cluster"), 
@@ -577,25 +590,25 @@ v <- ggplot(chatV, aes(x=DISTANCE_KM, y=M)) +
   labs(
     x = "Distance Downstream of Buford Dam [km]",
     y = "SSC [mg/L]",
-    title="Variable Cluster Assignment") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+    title="Variable Cluster Assignment")
+  #theme(text = element_text(family = "JetBrains Mono NL"))
 
-ggarrange(v, m, ncol = 2, widths=c(1,1))
+ggarrange(v, m, ncol = 2, widths=c(1,1), labels = c('(a)','(b)'))
 # ggsave(m, filename=paste0(wd_exports, "chatM.png"), height = 5, width = 7, units = "in", dpi=300)
 ggsave(filename=paste0(wd_exports, "chat.png"), height = 5, width = 10, units = "in", dpi=300)
 
 ##################### Fig 6 - Chattahoochee 2 ##################################
 # Majority cluster
-chatM <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/figure-data/CHAT4.csv")
+chatM <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/sentinel-ssc/figure-data/CHAT4.csv")
 # Add label for every 20 km
 chatM$dist_group <- floor(chatM$DISTANCE_KM/10)*10
 # USGS data
-usgs <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/figure-data/PROCESSED_CHATTAHOOCHEE_SSC.csv")
+usgs <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/sentinel-ssc/figure-data/PROCESSED_CHATTAHOOCHEE_SSC.csv")
 usgs$dist_group <- floor(usgs$dist_downstream_km/10) * 10
 # Filter from data 2000 - now
 usgs <- usgs %>% filter(sample_dt >= 1990)
 # Load dams
-dams <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/figure-data/ERDC_CHATTAHOOCHEE_DAMS.csv")
+dams <- read.csv("/Users/ilanvalencius/Documents/River-Sed-Manuscript/sentinel-ssc/figure-data/ERDC_CHATTAHOOCHEE_DAMS.csv")
 
 # Active sites
 active <- usgs %>% filter(sample_dt > 2022) %>% select(dist_downstream_km) %>% unique()
@@ -609,10 +622,10 @@ active <- usgs %>% filter(sample_dt > 2022) %>% select(dist_downstream_km) %>% u
 
 ggplot(chatM, aes(group=dist_group, x=dist_group, y=PRED_SSC_MGL)) +
   geom_boxplot(aes(color='Sentinel-2'), outlier.shape=NA) +
-  geom_boxplot(data=usgs, aes(y=SSC_mgL, color='USGS'), width=20, outlier.shape = NA) +
-  geom_vline(xintercept = dams$distance_km, color="red", linewidth=2) +
+  geom_boxplot(data=usgs, aes(y=SSC_mgL, color='USGS'), width=20, outlier.shape = NA, alpha=0.5, linewidth=0.75) +
+  geom_vline(xintercept = dams$distance_km, color="red", linewidth=1) +
   coord_cartesian(ylim = c(0, 150)) +
-  geom_point(data=active, aes(x=dist_downstream_km, y=0, group=NA), fill="purple", size=10, shape=24) +
+  geom_point(data=active, aes(x=dist_downstream_km, y=0, group=NA), fill="purple", size=5, shape=24) +
   theme_bw() +
   theme(legend.position = c(0.85, 0.9)) +
   scale_color_manual(values = c("#064012", "#3a3abb")) +
@@ -620,7 +633,7 @@ ggplot(chatM, aes(group=dist_group, x=dist_group, y=PRED_SSC_MGL)) +
   labs(
     x = "Distance Downstream of Buford Dam [km]",
     y = "SSC [mg/L]",
-    title="SSC Across the Chattahoocee and Apalachicola Rivers") +
-  theme(text = element_text(family = "JetBrains Mono NL"))
+    title="SSC Across the Chattahoocee and Apalachicola Rivers")
+  #theme(text = element_text(family = "JetBrains Mono NL"))
 
 ggsave(filename=paste0(wd_exports, "chat_boxplot.png"), height = 7, width = 10, units = "in", dpi=300)
